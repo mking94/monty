17fb278d952632b706ad8f4a1a8b1ee42771680e
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-22 09:01:59 -0400

    Header file

--- monty.h
+++ monty.h
@@ -1,21 +1,26 @@
-#ifndef MONTY_H_
-#define MONTY_H_
+#ifndef MONTY_H
+#define MONTY_H
+
 extern int data;
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 typedef struct stack_s
 {
 int n;
 struct stack_s *prev;
 struct stack_s *next;
 } stack_t;
+
 typedef struct instruction_s
 {
 char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void find_inst(char **tokens, stack_t **stack, unsigned int line_number);
+
 #endif
