97433e7d0b1f9f734861657833a1ba8f27e9d1f8
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-22 10:39:43 -0400

    Add files via upload

--- monty.h
+++ monty.h
@@ -1,13 +1,15 @@
-#ifndef MONTY
-#define MONTY
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-extern int stack_value;
-
-/* Includes */
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+extern char *line;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -16,53 +18,44 @@ extern int stack_value;
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO DHK project
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO DHK project
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-/* Functions Prototypes */
-void get_instructions(char **tokens, char *buffer,
-stack_t **stack, unsigned int line_number, FILE *fd);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-char **tokenizer(char *str);
-void pint(stack_t **stack, unsigned int line_number);
+/**********Function_operation***********************/
 void pop(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+/***********Arithmetique_Functio*******************/
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
+void division(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void _stack(stack_t **stack, unsigned int line_number);
-void _queue(stack_t **stack, unsigned int line_number);
-
-
-/* stack helpers functions */
+/*************Other Function**********************/
+void get_func(stack_t **stack, unsigned int line_number, char *opcode);
+char *get_opcode(void);
 void free_stack(stack_t *stack);
-size_t stack_len(const stack_t *stack);
-int delete_dnodeint_at_index(stack_t **stack, unsigned int index);
-
 #endif
